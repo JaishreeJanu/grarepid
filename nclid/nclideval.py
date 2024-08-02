7285c8caefe4057b14cb59bcb735f2b312e9a34e
janujaishree <janujaishree94@gmail.com>
2024-08-02 22:36:48 +0200

    add nclideval for random graphs

--- nclid/nclideval.py
+++ nclid/nclideval.py
@@ -5,7 +5,7 @@ NCLID evaluation
 
 author: svc@dmi.uns.ac.rs
 """
-
+import os
 
 from common.dataset_pool import DatasetPool
 from evaluation.lid_eval import NCLIDEstimator
@@ -15,67 +15,68 @@ from datetime import datetime
 from statistics import mean, stdev
 from scipy.stats import skew
 import json
+from tqdm import tqdm
 
-datasets = [
-    "karate_club_graph",
-    "les_miserables_graph",
-    "florentine_families_graph",
-    "cora_ml",
-    # "citeseer",
-    # "amazon_electronics_photo",
-    # "amazon_electronics_computers",
-    # "pubmed",
-    # "cora",
-    # "dblp"
-]
+import torch
+from torch_geometric.utils.convert import to_networkx
+from common.graph import Graph
+from torch_geometric.datasets import Planetoid, OGB_MAG, Amazon, CitationFull, TUDataset
 
+datasets = ["SYNTHETIC", "MSRC_9", "MSRC_21", "MSRC_21C", "Fingerprint", "FIRSTMM_DB", "mit_ct1", "mit_ct2",
+            "Mutagenicity", "AIDS", "Tox21_ARE", "MUTAG", "ENZYMES", "PROTEINS",
+            "TWITTER-Real-Graph-Partial", "TRIANGLES", "COLORS-3"]
+
+# "MSRC_9", "MSRC_21", "MSRC_21C", "Fingerprint", "FIRSTMM_DB", "mit_ct1", "mit_ct2", "Mutagenicity", "AIDS", "Tox21_ARE", "MUTAG", "ENZYMES", "PROTEINS", "TWITTER-Real-Graph-Partial"
+# "mit_ct1", "mit_ct2": Error: "expected sequence of length 4 at dim 1 (got 2)"
 if __name__ == "__main__":
     from os import sys
     print(datetime.now())
     print("Evaluating natural communities")
-    
-    print("DATASET,AVG-NCLID,STD-NCLID,CV-NCLID,SKW-NCLID,MIN-NCLID,MAX-NCLID")
-    tl = []
-    for d in datasets:
-        graph = DatasetPool.load(d)
+
+    # if d in {"Cora", "CiteSeer", "PubMed"}:
+    #     data = Planetoid(name=d, root="data/")
+    # elif d in {"Computers", "Photo"}:
+    #     data = Amazon(name=d, root="../torch_datasets/")
+    graph_meta_dict = []
+    graph_id_dict = []
+    graph_nclen_dict = []
+    for d in tqdm(os.listdir('../random_graphs/rp_sbm/')):
+        data = torch.load(f'../random_graphs/rp_sbm/{d}')
+
+        graph = Graph(to_networkx(data, node_attrs=['x', 'y'], to_undirected=False))
+
+        # graph_meta_dict.append({'graph': d,
+        #                         'num_nodes': data.num_nodes,
+        #                         'num_edges': data.num_edges,
+        #                         'num_features': data.num_node_features,
+        #                         'is_undirected': data.is_undirected(),
+        #                         'avg_node_degree': 2 * (data.num_edges) / (data.num_nodes)})
+        # graph = DatasetPool.load(d)
         graph.remove_selfloop_edges()
         graph = graph.to_undirected()
 
-        nclid = NCLIDEstimator(graph)
-        nclid.estimate_lids()
+        try:
+            nclid = NCLIDEstimator(graph)
+            nclid.estimate_lids()
+        except Exception as e:
+            print("Exception OCCURRED in NCLID Algo: ", e)
+            continue
         nclids = list(nclid.get_lid_values().values())
-        with open(d+'all_nclids.json', 'w') as fp:
-            json.dump(nclid.get_lid_values(), fp)
+        graph_id_dict.append({'graph': d,
+                              'avg_nclid': mean(nclids),
+                              'min_nclid': min(nclids),
+                              'max_nclid': max(nclids)})
         nclens = [nclid.nc_len(node[0]) for node in graph.nodes()]
-        with open(d+'all_nclens.txt', 'w') as fp:
-            fp.write(f"{nclens}\n")
-        
-        avg_nclid = mean(nclids)
-        std_nclid = stdev(nclids)
-        cv_nclid  = std_nclid / avg_nclid
-        min_nclid = min(nclids)
-        max_nclid = max(nclids)
-        skw_nclid = skew(nclids)
 
-        s = d + "," + str(avg_nclid) + "," + str(std_nclid) + "," + str(cv_nclid) + "," + str(skw_nclid) + ","
-        s += str(min_nclid) + "," + str(max_nclid)
+        graph_nclen_dict.append({'graph': d,
+                              'avg_nclen': mean(nclens),
+                              'min_nclen': min(nclens),
+                              'max_nclen': max(nclens)})
 
-        print(s)
 
-        avg_nclens = mean(nclens)
-        std_nclens = stdev(nclens)
-        cv_nclens = std_nclens / avg_nclens
-        min_nclens = min(nclens)
-        max_nclens = max(nclens)
-        skw_nclens = skew(nclens)
+    with open(f'./torch_datasets/random_graphs/rp_sbm_nc_LID.json', 'w') as fp:
+        json.dump(graph_id_dict, fp)
 
-        t = d + "," + str(avg_nclens) + "," + str(std_nclens) + "," + str(cv_nclens) + "," + str(skw_nclens) + ","
-        t += str(min_nclens) + "," + str(max_nclens)
-        tl.append(t)
-
-    print("\n")
-    print("DATASET,AVG-NCLEN,STD-NCLEN,CV-NCLEN,SKW-NCLEN,MIN-NCLEN,MAX-NCLEN")
-    for t in tl:
-        print(t)
-
-    print(datetime.now())
+    with open(f'./torch_datasets/random_graphs/rp_sbm_nclen.json', 'w') as fp:
+        json.dump(graph_nclen_dict, fp)
+print(datetime.now())
